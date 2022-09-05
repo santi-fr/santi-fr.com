db3e2c25c31f457a414e07d7ba7af3ce7f80c32a
santi-fr <santi.fr@icloud.com>
2022-09-05 18:45:21 +0200

    Header
    - Added some stuff to the header

---
+++ src/components/header/Buttons.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Buttons = () => {
+  return (
+    <div className='buttons'>
+        <a href='../../images/cv.pdf' className='btn'>Download CV</a>
+        <a href='#contact' className='btn btn-primary'>Contact me</a>
+    </div>
+  )
+}
+
+export default Buttons
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './header.css'
+import Buttons from './Buttons'
+import Socials from './Socials'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>My name is</h5>
+        <h1>Santiago Fonseca</h1>
+        <h5 className='text-light'>Software Developer &amp; Computer Science and Engineering Student </h5>
+        <Buttons/> 
+        <Socials/>
+        <div className='me'>
+          <img src="../../images/me" alt="Me"></img>
+        </div>
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
